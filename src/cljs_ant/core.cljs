6a6d23eb82aec682b0a4951809915e41b7d6d64f
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2015-02-16 14:04:37 +0100

    Set world width and height

--- src/cljs_ant/core.cljs
+++ src/cljs_ant/core.cljs
@@ -6,8 +6,10 @@
 (println "Edits to this text should show up in your developer console.")
 
 (def refresh-rate 10) ; miliseconds
+(def world-width (-> (. document body) (. clientWidth)))
+(def world-height (-> (. document body) (. clientHeight)))
 
-(defonce world (atom {:paper (js/Raphael 0 0 500 500)}))
+(defonce world (atom {:paper (js/Raphael 0 0 world-width world-height)}))
 
 (defn draw-world []
   (let [paper (:paper @world)]
