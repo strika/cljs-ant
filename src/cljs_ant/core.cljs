08d425a415bd680bc74e69651ccf84f14197be29
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2015-02-21 15:51:39 +0100

    Pull out world-view

--- src/cljs_ant/core.cljs
+++ src/cljs_ant/core.cljs
@@ -1,14 +1,11 @@
 (ns ^:figwheel-always cljs-ant.core
-    (:require))
+  (:require [cljs-ant.world-view :refer [draw-world world-width world-height]]))
 
 (enable-console-print!)
 
 (println "Edits to this text should show up in your developer console.")
 
 (def refresh-rate 200) ; miliseconds
-(def ant-size 10) ; px
-(def world-width (-> (.-body js/document) (.-clientWidth)))
-(def world-height (-> (.-body js/document) (.-clientHeight)))
 
 (defn random-ant-direction []
   :up)
@@ -19,28 +16,17 @@
    :direction (random-ant-direction)})
 
 (defn generate-world []
-  {:paper (js/Raphael 0 0 world-width world-height)
-   :live-cells []
+  {:live-cells []
    :ant (generate-ant world-width world-height)})
 
 (defonce world (atom (generate-world)))
 
-(defn draw-cells [paper live-cells]
-  (doseq [cell live-cells]
-    (. paper rect (:x cell) (:y cell) ant-size ant-size)))
-
-(defn draw-world []
-  (let [paper (:paper @world)
-        live-cells (:live-cells @world)]
-    (. paper clear)
-    (draw-cells paper live-cells)))
-
-(defn update-world []
-  (swap! world assoc :live-cells [{:x 100 :y 100} {:x 200 :y 200}]))
+(defn update-world [world]
+  (swap! world assoc :live-cells [{:x 100 :y 100} {:x 100 :y 200}]))
 
 (defn loop-world []
-  (update-world)
-  (draw-world))
+  (update-world world)
+  (draw-world (:live-cells @world)))
 
 (defonce create-world
   (do
---
+++ src/cljs_ant/world_view.cljs
@@ -0,0 +1,22 @@
+(ns ^:figwheel-always cljs-ant.world-view
+  (:require))
+
+(def ant-size 10) ; px
+(def ant-color "#00f")
+(def world-width (-> (.-body js/document) (.-clientWidth)))
+(def world-height (-> (.-body js/document) (.-clientHeight)))
+
+(defn generate-paper []
+  (js/Raphael "paper" world-width world-height))
+
+(defonce view (atom {:paper (generate-paper)}))
+
+(defn draw-cells [paper live-cells]
+  (doseq [cell live-cells]
+    (-> (. paper rect (:x cell) (:y cell) ant-size ant-size)
+        (. attr "fill" ant-color))))
+
+(defn draw-world [live-cells]
+  (let [paper (:paper @view)]
+    (. paper clear)
+    (draw-cells paper live-cells)))
